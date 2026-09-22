b2ef46115c1e66cce522b2a15d037ceb99f7c7d9
badrouhk2-ctrl <badrouhk2@gmail.com>
2026-09-22 11:26:39 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,3 @@
 import "./globals.css";
-
-export const metadata = {
-  title: "CYBER MODZ",
-  description: "FiveM Mods Store",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+export const metadata={title:"CYBER MODZ",description:"FiveM Mods Store"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
